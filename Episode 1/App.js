b3d2315fbf75afe13cca3646ccb1cc175c39bab1
metinbicaksiz <metinbicaksiz@gmail.com>
2024-07-15 02:08:41 +0300

    jsx changes

--- Episode 1/App.js
+++ Episode 1/App.js
@@ -1,18 +1,7 @@
-const parent = React.createElement("div", { id: "parent"},
-    [
-        React.createElement("div", {id: "child"},
-        [
-            React.createElement("h1", {}, "H1"),
-            React.createElement("h2", {}, "h2"),
-        ]),
-        React.createElement("div", {id: "child2"},
-            [
-                React.createElement("h1", {}, "H1"),
-                React.createElement("h2", {}, "h2"),
-            ])
-    ]
-);
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+import React from 'react';
+import ReactDOM from'react-dom';
 
-console.log(heading);
+const headingJsx = <h1 id="heading">Namaste React!!!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headingJsx);
